f232de6e0bdad4537f4d13ab452c1b74c627619b
Cristiam Díaz <c.diaz@udla.edu.co>
2020-02-29 11:29:32 -0500

    feat(usuarios): crud de usuarios

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//require('dotenv').config({path: path.resolve(__dirname+'.env')});
 let exphbs = require('express-handlebars')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,51 @@ router.get('/', async (req, res, next) => {
   res.json(result);
 });
 
+/* Obtener un usuario. */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const result = await query('SELECT * FROM usuario WHERE id_usuario = ?', [req.params.id]);
+    res.json(result);
+  }
+  catch (error) {
+    console.log('Error =>', error);
+    res.send(error.sqlMessage);
+  }
+});
+
+/* Registrar usuario. */
+router.post('/', async (req, res, next) => {
+  var { cedula, nombres, apellidos, telefono, mail, password, rol } = req.body;
+  try {
+    const result = await query("INSERT INTO usuario (cedula, nombres, apellidos, telefono, mail, password, fk_id_rol) VALUES (?, ?, ?, ?, ?, ?, ?)", [cedula, nombres, apellidos, telefono, mail, password, rol]);           
+    res.json(result);
+  } catch (error) {
+    console.log('Error =>', error);
+    res.send(error.sqlMessage);
+  }
+});
+
+/* Actualizar usuario. */
+router.put('/:id', async (req, res, next) => {
+  var { cedula, nombres, apellidos, telefono, mail, password, rol } = req.body;
+  try {
+    const result = await query(`UPDATE usuario SET id_usuario = ${req.params.id}, cedula = ${cedula}, nombres = '${nombres}', apellidos = '${apellidos}', telefono = '${telefono}', mail = '${mail}', password = '${password}', fk_id_rol = ${rol} WHERE id_usuario = ${req.params.id}`);           
+    res.json(result);
+  } catch (error) {
+    console.log('Error =>', error);
+    res.send(error.sqlMessage);
+  }
+});
+
+/* Eliminar usuario. */
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const result = await query(`UPDATE usuario SET activo = 0 WHERE id_usuario = ${req.params.id}`);      
+    res.json(result);     
+  } catch (error) {
+    console.log('Error =>', error);
+    res.send(error.sqlMessage);
+  }
+});
 
 module.exports = router;
