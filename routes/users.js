300d51f6843d40c11b90b7616d238368254dde1c
yuly cruz <yul.cruz@udla.edu.co>
2020-03-02 04:27:47 +0100

    listar usuarios

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const query = util.promisify(db.query).bind(db);
 router.get('/', async (req, res, next) => {
   const result = await query('SELECT * FROM usuario');
     
-  res.render('admin/empleadosyproovedores', { categorias: result, layout: 'admin' })
+  res.render('admin/usuario', { usuarios: result, layout: 'admin' })
 });
 
 /* Obtener un usuario. */
