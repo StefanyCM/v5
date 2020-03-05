2d7967dd45af02cbfc6a4012f13509f14532fd22
Cristiam Díaz <c.diaz@udla.edu.co>
2020-03-05 09:34:41 -0500

    fix change upload directory

--- utils/multer-storage.js
+++ utils/multer-storage.js
@@ -3,7 +3,7 @@ const uuidv4 = require('uuid/v4')
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, 'public/uploads/')
   },
   filename: (req, file, cb) => {
     cb(null, uuidv4()+file.originalname.substring(file.originalname.indexOf(".")));
