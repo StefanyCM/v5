062fd6486a308f72db345e3fad30a888fa0df066
yuly cruz <yul.cruz@udla.edu.co>
2020-03-05 07:11:50 +0100

    feat (filtrar marcas, categorias)

--- routeStore/index.js
+++ routeStore/index.js
@@ -3,19 +3,30 @@ var router = express.Router();
 const util = require('util');
 const db = require('../utils/database');
 
-// Promesas nativas
 const query = util.promisify(db.query).bind(db);
 
-/* GET home page. */
 router.get('/',async function(req, res, next) {
- 
-  const result = await query('select * from producto');
+
+  var result;
+  if(req.query.marca){
+    console.log("si trae marca" + req.query.marca)
+    result = await query('select * from producto WHERE fk_id_marca = ?', [req.query.marca]);
+  } else {
+    console.log("no trae marca")
+    result = await query('select * from producto');
+  }
+
+ if(req.query.categoria){
+  console.log("si trae categoria" + req.query.categoria)
+    result = await query('select * from producto WHERE fk_id_categoria = ?', [req.query.categoria]);
+  } else {
+    console.log("si trae categoria" + req.query.categoria)
+    result = await query('select * from producto');
+  }
   const categorias = await query('SELECT * FROM categoria');
   const marcas = await query('SELECT * FROM marca');
 
  res.render('index', { productos: result, categorias: categorias, marcas:marcas, layout: 'main', title: 'Dashboard'});
-
-
 });
 
 module.exports = router;
